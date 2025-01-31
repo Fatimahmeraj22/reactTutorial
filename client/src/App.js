8e366c6facde2fd0f0525f16a6e8eeb38ea75bca
Mac <mac@Meraj.local>
2025-01-31 17:18:24 +0530

    reactTutorial

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Myname from './myname'
+
+function App() {
+  return (
+    <div>HElloo devss!!!
+      <Myname />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/myname.js
@@ -0,0 +1,8 @@
+function Myname () {
+    return (
+        <div><h1>YOURRRRRR NAME?????</h1>
+        </div>
+    )
+}
+
+export default Myname;
